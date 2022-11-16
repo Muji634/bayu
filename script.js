ff66d4bf30e192557c3ea395cf80380db11ec6d4
bayu <bmuji634@gmail.com>
2022-11-16 14:11:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let tkTitle = document.getElementById("tk-title")
+let sdTitle = document.getElementById("sd-title")
+let smpTitle = document.getElementById("smp-title")
+let smkTitle = document.getElementById("smk-title")
+
+function hover1 () {
+    tkTitle.classList.toggle("tk")
+}
+function out1() {
+    tkTitle.classList.remove("tk")
+}
+
+function hover2 () {
+    sdTitle.classList.toggle("sd")
+}
+function out2() {
+    sdTitle.classList.remove("sd")
+}
+
+function hover3 () {
+    smpTitle.classList.toggle("smp")
+}
+function ou3() {
+    smpTitle.classList.remove("smp")
+}
+
+function hover4 () {
+    smkTitle.classList.toggle("smk")
+}
+function out4() {
+    smkTitle.classList.remove("smk")
+}
